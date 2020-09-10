518d07908d85c27a96951651bae87d6816b16f31
nero2310 <nero2310@wp.pl>
2020-09-11 01:30:01 +0200

    Add base recipe model

--- Recipes/models.py
+++ Recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=200) # toDo If user aren't specify set user name to Anonymous
+    ingredients = models.JSONField(blank=False)
