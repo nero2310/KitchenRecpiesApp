67bee8298309a8ee79feed37ea13be05a4872dea
nero2310 <nero2310@wp.pl>
2020-09-11 23:19:42 +0200

    Add base template

--- Recipes/views.py
+++ Recipes/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'Recipes/index.html')
+    return render(request, 'Recipes/index.html', {"title": "Index"})
