c209e161b028fc2ccd65b433d64a0418b3c7fda1
nero2310 <nero2310@wp.pl>
2020-09-12 14:24:45 +0200

    Printing recipes content from db

--- KitchenRecpiesApp/urls.py
+++ KitchenRecpiesApp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Recipes.urls"))
+    path('recipes/', include("Recipes.urls"))
 ]
--- Recipes/views.py
+++ Recipes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Recipe
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'Recipes/index.html', {"title": "Index"})
+    query = Recipe.objects.get_queryset()
+    return render(request, 'Recipes/index.html', context=query)
