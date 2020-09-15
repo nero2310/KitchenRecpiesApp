c2d42a3959bc105811f9a5a67ba69531b5973cda
nero2310 <nero2310@wp.pl>
2020-09-15 18:36:25 +0200

    Change in Recipe model
    Change JsonField to ArrayField

--- Recipes/models.py
+++ Recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 # Create your models here.
@@ -8,4 +9,4 @@ class Recipe(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now=True)
     author = models.CharField(max_length=200) # toDo If user aren't specify set user name to Anonymous
-    ingredients = models.JSONField(blank=False)
+    ingredients =  ArrayField(models.CharField(max_length=100,blank=True))
