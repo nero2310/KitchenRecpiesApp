3a23dbd2ad372f1dd683d6e8f72fcc207ede0756
nero2310 <nero2310@wp.pl>
2020-09-17 13:23:10 +0200

    Add comment model

--- Recipes/urls.py
+++ Recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from  Recipes.views import Index,CreateRecipe
+from Recipes.views import Index,CreateRecipe,ShowRecipe
 
 from . import views
 
 urlpatterns = [
     path('', Index.as_view(), name="index"),
+    path('<int:pk>/',ShowRecipe.as_view(), name="ShowRecipe"),
     path('create',CreateRecipe.as_view(), name="CreateRecipe")
 ]
