b687a6e5eb0f6d9c8f0e6a72d68322564e19d9dd
nero2310 <nero2310@wp.pl>
2020-09-19 11:33:36 +0200

    Add working comment model,squash migrations
    Comment model need refactoring, Probably move to new application

--- Recipes/views.py
+++ Recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView
-from .models import Recipe
+from .models import Recipe,Comment
 from .forms import CreateRecipeForm,CreateCommentForm
 from  django.shortcuts import redirect
+from django.views.generic.edit import FormView
 
 # Create your views here.
 
@@ -18,16 +19,23 @@ class CreateRecipe(CreateView):
     template_name = "Recipes/create_recipe_form.html"
 
 
-class ShowRecipe(DetailView):
+class ShowRecipe(FormView):
     template_name = "Recipes/show_recipe.html"
-    model = Recipe
+    form_class = CreateCommentForm
+    success_url = '/recipes'
+
+    def get_object(self):
+        return Recipe.objects.filter(pk=self.kwargs['pk'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["recipe"] = Recipe.objects.filter(pk=self.kwargs["pk"])
-        context["comments"] = Recipe.objects.filter(pk=self.kwargs["pk"])
-        context["form"] = CreateCommentForm()
+        context["recipe"] = self.get_object()
+        context["comments"] = Comment.objects.filter(recipe_id=self.kwargs["pk"])
         return context
 
-    def post(self,request,pk=0):
-        return redirect(ShowRecipe,pk=self.kwargs["pk"])
+    def form_valid(self, form):
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.recipe_id = self.get_object()
+            comment.save()
+        return super().form_valid(form)
