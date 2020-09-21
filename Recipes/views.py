ec078732267e35b696f0fc284a388e9190d2ce5c
nero2310 <nero2310@wp.pl>
2020-09-21 14:11:25 +0200

    BugFix:Repair broken recipes displaying
    Modify template to reflect models changes

--- Recipes/views.py
+++ Recipes/views.py
@@ -25,7 +25,7 @@ class ShowRecipe(FormView):
     success_url = '/recipes'
 
     def get_object(self):
-        return Recipe.objects.filter(pk=self.kwargs['pk'])
+        return Recipe.objects.get(pk=self.kwargs['pk'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
