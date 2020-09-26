ea003979f8885118aa693c844bc3375aa54a32fa
nero2310 <nero2310@wp.pl>
2020-09-26 15:59:08 +0200

    User will be login after valid signup

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+
+
 # Create your views here.
 
 
@@ -8,6 +11,15 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')  # redirect to main page
     else:
         form = UserCreationForm()
-    return render(request,'auth/signup.html',{'form':form})
+    return render(request, 'auth/signup.html', {'form': form})
+
+
+def user_login(request):
+    pass
