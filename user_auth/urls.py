c25191ece863e7e49d1783424e725338b03b40f5
nero2310 <nero2310@wp.pl>
2020-09-27 14:26:19 +0200

    Add login system

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name="signup"),
-    path('login', views.login, name="login"),
+    path('register', views.signup, name="signup"),
+    path('login', views.UserLogin.as_view(), name="login"),
     path('logout', views.logout_view, name="logout"),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import View
 
 # Create your views here.
 
@@ -15,14 +15,27 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')  # redirect to main page
+            return redirect('Recipes:index')  # redirect to main page
     else:
         form = UserCreationForm()
     return render(request, 'auth/signup.html', {'form': form})
 
 
-def user_login(request):
-    pass
+class UserLogin(View):
+    form_class = AuthenticationForm
+    template_name = 'auth/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("Recipes:index")
+        else:
+            return render(request, self.template_name, {'form': form})
 
 
 def logout_view(request):
