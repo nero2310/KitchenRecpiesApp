6f5376ebf33ade5e97374637763041267607dc51
nero2310 <nero2310@wp.pl>
2020-10-01 03:01:11 +0200

    Create base test,whose test login system

--- user_auth/tests.py
+++ user_auth/tests.py
@@ -1,13 +1,21 @@
-from django.test import TestCase,Client,override_settings
-from unittest.mock import patch
+from django.test import TestCase,Client,override_settings,RequestFactory
+from django.contrib.auth.models import User
+
+from os import environ
+
 from .views import UserLogin
 
 # Create your tests here.
 
 class LoginTestCase(TestCase):
     def setUp(self):
-        client = Client()
+        self.client = Client()
+        self.user = User.objects.create_user(username="Jhon",password="very_hard_password")
 
-    @override_settings(POSTGRES_PASSWORD="example")
+    @override_settings(POSTGRES_PASSWORD=environ["POSTGRES_PASSWORD"])
     def test_user_login(self):
-        self.client.post(path="/auth/login")
+        request = self.client.post(path="/auth/login")
+        request.user = self.user
+        view = UserLogin()
+        view.setup(request)
+
