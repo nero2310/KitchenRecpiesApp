559195c4fbbd04fd6ecb9d275d81534e4664417c
nero2310 <nero2310@wp.pl>
2020-10-05 02:02:30 +0200

    Testing adding few comments

--- Recipes/tests/tests_views.py
+++ Recipes/tests/tests_views.py
@@ -12,6 +12,10 @@ class ShowRecipeViewTest(TestCase):
                                             , prepare_dish_recipe="1.Prepare ingredients 2.Make my unit test working"
                                                                   "2.Make my unit test working")
 
+    def create_comment(self, view, data):
+        form = CreateCommentForm(data=data)
+        return view.form_valid(form)
+
     def test_comment_creation(self):  # Testing comment creation and created comment content
         request = RequestFactory().get("/")
         recipe = self.recipe
@@ -20,3 +24,14 @@ class ShowRecipeViewTest(TestCase):
         comments = view.get_context_data()["comments"]
         assert (view.form_valid(form))
         self.assertEqual(first=[comment.content for comment in comments], second=["test_content"])
+
+    def test_few_comments_creation(self):
+        request = RequestFactory().get("/")
+        recipe = self.recipe
+        view = ShowRecipe(request=request, kwargs={"pk": recipe.pk})
+        self.create_comment(view, data={"recipe_id": recipe.pk, "author": "test", "content": "test_content"})
+        self.create_comment(view, data={"recipe_id": recipe.pk, "author": "test", "content": "wut"})  # Comment will
+        self.create_comment(view, data={"recipe_id": recipe.pk, "author": "test", "content": "properly_comment"})
+        # Comment will not be created because content is too short
+        comments = view.get_context_data()["comments"]
+        self.assertEqual(first=[comment.content for comment in comments], second=["test_content", "properly_comment"])
