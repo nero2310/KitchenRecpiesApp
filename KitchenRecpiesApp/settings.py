7514e1d8e991454fb48b6693a2bdefb6f1f9fac3
nero2310 <nero2310@wp.pl>
2020-10-06 17:19:43 +0200

    WIP

--- KitchenRecpiesApp/settings.py
+++ KitchenRecpiesApp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 from os import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    "static"
+]
 # Application definition
 
 INSTALLED_APPS = [
